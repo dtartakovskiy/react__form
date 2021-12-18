890f2c1cb1f975e8d5ecf1db91b9d0a00f012c64
Dmytro Tartakovskyi <d.tartakovskiy@gmail.com>
2021-12-19 00:36:11 +0200

    finished form validation

--- src/App.jsx
+++ src/App.jsx
@@ -18,20 +18,28 @@ export const App = () => {
       name: 'username',
       type: 'text',
       placeholder: 'Name',
+      errorMessage:
+        "User name should be 3-16 chars and shouldn't include any special chars",
       label: 'User name',
+      required: true,
+      pattern: '^[A-Za-z0-9]{3,16}$',
     },
     {
       id: 2,
       name: 'email',
       type: 'text',
       placeholder: 'Email',
+      errorMessage: 'It should be a valid email adsress',
       label: 'Email',
+      required: true,
+      pattern: '[a-z0-9]+@[a-z]+.[a-z]{2,3}',
     },
     {
       id: 3,
       name: 'birthday',
-      type: 'text',
+      type: 'date',
       placeholder: 'Birthday',
+      errorMessage: '',
       label: 'Birthday',
     },
     {
@@ -39,14 +47,22 @@ export const App = () => {
       name: 'password',
       type: 'password',
       placeholder: 'Password',
+      errorMessage:
+        'It should be 8-20 chars and include at least 1 letter, 1 number and  special char',
       label: 'Password',
+      required: true,
+      pattern:
+        '^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$',
     },
     {
       id: 5,
       name: 'confirmPassword',
       type: 'password',
       placeholder: 'Confirm password',
+      errorMessage: "Password doesn't not match",
       label: 'Confirm password',
+      required: true,
+      pattern: values.password,
     },
   ]
 
@@ -61,12 +77,13 @@ export const App = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log('submitted')
+    console.log('Form values', JSON.stringify(values, null, 2))
   }
 
   return (
     <div className='app'>
       <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
         {inputs.map(({ id, ...rest }) => (
           <FormInput
             key={id}
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './formInput.css'
 
-export const FormInput = ({ label, handleChange, ...inputProps }) => {
+export const FormInput = ({
+  label,
+  errorMessage,
+  handleChange,
+  ...inputProps
+}) => {
+  const [focused, setFocused] = useState(false)
+
+  const handleFocus = () => setFocused(true)
+
   return (
     <div className='formInput'>
       <label>{label}</label>
-      <input {...inputProps} onChange={handleChange} />
+      <input
+        {...inputProps}
+        onChange={handleChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === 'confirmPassword' && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
     </div>
   )
 }
